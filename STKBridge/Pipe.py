c9d9901c21e523be7889e8301dc088dad2b96552
Desktop W10 <robertodelprete88@gmail.com>
2022-03-28 16:42:22 +0200

    commit

--- STKBridge/Pipe.py
+++ STKBridge/Pipe.py
@@ -10,27 +10,27 @@ class Pipeline(STK):
           print("STK initialized.")
      
 
-     def genSol(self, X: list):
-          assert(len(X)==6)
+     # def genSol(self, X: list):
+     #      assert(len(X)==6)
           
-          a, i, RAAN, NumPlanes, NumSatxPlane, RAANincerement = X 
-          nSats = NumPlanes * NumSatxPlane
+     #      a, i, RAAN, NumPlanes, NumSatxPlane, RAANincerement = X 
+     #      nSats = NumPlanes * NumSatxPlane
 
-          SeedDict = {'a':a, 'e':0,'i':i, 'w':0, 'RAAN':RAAN, 'M':10.5}
-          ConstDict = {'NumPlanes':NumPlanes, 'NumSatsPerPlane':NumSatxPlane, 'InterPlaneTrueAnomalyIncrement': 5, 'RAANIncrement': RAANincerement, 'ColorByPlane': 'Yes'}
-          #####################################################
-          super().addSatellite(SatName='mysat', params=SeedDict)
-          super().addSensor(SatName='mysat', SenName='Sensor1', params={'coneAngle':15, 'angularResolution':0.1, 'AzEl':[90,-90], 'maxRange':1200})
-          super().WalkerDelta(SatName='mysat', params=ConstDict)
-          super().CreateConstellationObject(ConstellationName='Sensors')
-          super().CreateChainObject(pathObjToAdd=['Constellation/AllUsers', 'Constellation/Sensors'], chainName='Chain')
-          usersList, AccessDict = super()._computeChain(chainName='Chain')
-          # mWin, mGap = evaluator(usersList, AccessDict)
-          # solution_old = [mWin, mGap, ConstDict['NumSatsPerPlane']*ConstDict['NumPlanes']]
-          # solution_old2 = user_evaluator(usersList, AccessDict, nSats)
-          score = double_evaluator(usersList, AccessDict, nSats) 
-          super()._reset()
-          return score # [score, nUsers]
+     #      SeedDict = {'a':a, 'e':0,'i':i, 'w':0, 'RAAN':RAAN, 'M':10.5}
+     #      ConstDict = {'NumPlanes':NumPlanes, 'NumSatsPerPlane':NumSatxPlane, 'InterPlaneTrueAnomalyIncrement': 5, 'RAANIncrement': RAANincerement, 'ColorByPlane': 'Yes'}
+     #      #####################################################
+     #      super().addSatellite(SatName='mysat', params=SeedDict)
+     #      super().addSensor(SatName='mysat', SenName='Sensor1', params={'coneAngle':15, 'angularResolution':0.1, 'AzEl':[90,-90], 'maxRange':1200})
+     #      super().WalkerDelta(SatName='mysat', params=ConstDict)
+     #      super().CreateConstellationObject(ConstellationName='Sensors')
+     #      super().CreateChainObject(pathObjToAdd=['Constellation/AllUsers', 'Constellation/Sensors'], chainName='Chain')
+     #      usersList, AccessDict = super()._computeChain(chainName='Chain')
+     #      # mWin, mGap = evaluator(usersList, AccessDict)
+     #      # solution_old = [mWin, mGap, ConstDict['NumSatsPerPlane']*ConstDict['NumPlanes']]
+     #      # solution_old2 = user_evaluator(usersList, AccessDict, nSats)
+     #      score = double_evaluator(usersList, AccessDict, nSats) 
+     #      super()._reset()
+     #      return score # [score, nUsers]
 
      def genSol2(self, X: list, reset=True):
           assert(len(X)==4)
@@ -53,14 +53,14 @@ class Pipeline(STK):
           SeedDict = {'a':a1, 'e':0,'i':i1, 'w':0, 'RAAN':RAAN1, 'M':10.5}
           ConstDict = {'NumPlanes':NumPlanes, 'NumSatsPerPlane':NumSatxPlane, 'InterPlaneTrueAnomalyIncrement': 0, 'RAANIncrement': RAANincerement, 'ColorByPlane': 'Yes'}
           super().addSatellite(SatName='1seed', params=SeedDict)
-          super().addSensor(SatName='1seed', SenName='Sensor1', params={'coneAngle':60, 'angularResolution':0.1, 'AzEl':[90,-90], 'maxRange':1200})
+          super().addSensor(SatName='1seed', SenName='Sensor1', params={'coneAngle':50, 'angularResolution':0.1, 'AzEl':[90,-90], 'maxRange':1200})
           super().WalkerDelta(SatName='1seed', params=ConstDict)
 
           ##################################################### 2nd Walker
           SeedDict = {'a':a2, 'e':0,'i':i2, 'w':0, 'RAAN':RAAN2, 'M':10.5}
           ConstDict = {'NumPlanes':NumPlanes, 'NumSatsPerPlane':NumSatxPlane, 'InterPlaneTrueAnomalyIncrement': 0, 'RAANIncrement': RAANincerement, 'ColorByPlane': 'Yes'}
           super().addSatellite(SatName='2seed', params=SeedDict)
-          super().addSensor(SatName='2seed', SenName='Sensor1', params={'coneAngle':60, 'angularResolution':0.1, 'AzEl':[90,-90], 'maxRange':1200})
+          super().addSensor(SatName='2seed', SenName='Sensor1', params={'coneAngle':50, 'angularResolution':0.1, 'AzEl':[90,-90], 'maxRange':1200})
           super().WalkerDelta(SatName='2seed', params=ConstDict)
 
           ##################################################### Constellation + Chain
@@ -70,7 +70,7 @@ class Pipeline(STK):
           # mWin, mGap = evaluator(usersList, AccessDict)
           # solution_old = [mWin, mGap, ConstDict['NumSatsPerPlane']*ConstDict['NumPlanes']]
           # solution_old2 = user_evaluator(usersList, AccessDict, nSats)
-          score, nUsers = double_evaluator(usersList, AccessDict, nSats)
+          score, nUsers = winMean_evaluator(usersList, AccessDict, nSats)
           if reset: 
                super()._reset()
           return [score, nUsers] 
