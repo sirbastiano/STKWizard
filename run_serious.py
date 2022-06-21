d0c3e346c22ed5ad1cf81a28ffa41059aad16872
Roberto Del Prete <robertodelprete88@gmail.com>
2022-06-21 10:15:19 +0200

    commit all

--- run_serious.py
+++ run_serious.py
@@ -22,15 +22,15 @@ def main():
                     super().duplicateRemover()
                     # parms: SELECT --> 0: WinMean || 1: WinMultiple || 2: Double Users || 3: striano_evaluator
                     # super().evaluate(function=lambda x: f.genSol2(x, SELECT = 3, CONE_ANGLE = 20))
-                    super().evaluate(function=lambda x: f.genSol3(x, SELECT = 3, CONE_ANGLE = 20))
+                    super().evaluate(function=lambda x: f.genSol3(x, SELECT = 3, CONE_ANGLE = 15))
                     super().survival(sortBy='Eval')
                     print(f'Gen {self.idxGen} completed.')
                     self.idxGen += 1
 
      
      Alg = Optimizer(popSize=100, nGenerations=20, nVar=6, nObj=1, toKeep=5, nParents=20, toRand=30)
-     #Alg.run(startWith=2)
-     Alg.run()
+     Alg.run(startWith=9)
+     # Alg.run()
 
 
 if __name__ == '__main__':
